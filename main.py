d6a2f82da112101262e12c4840ece3ab1e7b9aff
Junfel <junfeldev@gmail.com>
2025-08-19 15:55:11 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+
+# def main():
+#     name = input("Enter your name: ")
+#     age = input("Enter your age: ")
+    
+#     print("Hello", name, "You are now", age, "years old")
+    
+# main()
+
+# a = [1, 52, 3, 12, 4, 52]
+# print(a.count(52))
+
+# username = input("What is your name? ")
+# length = len(username)
+
+# print(length)
+
+
+# glass1 = "milk"
+# glass2 = "juice"
+
+# new_glass = glass1
+# glass1 = glass2
+# glass2 = new_glass
+
+# print("GLASS1: ", glass1)
+# print("GLASS2: ", glass2)
+
+# print("--------------------------------------------------")
+# print("Hello and welcome to Band Name Generator!")
+# print("--------------------------------------------------")
+# city = input("What city did you grew up?\n")
+# pet = input("What is your pet's name?\n")
+# print("Your band name is:", city + " " + pet)
+
+
+print(type(123_456.5))
+print(type("stringasdf"))
+print(type(True))
