33dc99d89115c1d6bf50b2237dd7684e69b7eabf
Junfel <vjunfel@gmail.com>
2025-08-25 14:58:07 +0800

    Simplified

--- password_generator.py
+++ password_generator.py
@@ -10,15 +10,14 @@ def random_password_generator():
     password = ""
 
     number_of_characters = int(input("How many characters do you want? "))
-    if (len(password) < number_of_characters):
-        for _ in range(number_of_characters):
-            num = str(random.randint(0, 9))
-            letter = str(alphabeth[random.randint(0, len(alphabeth) - 1)])
-            spec = str(special[random.randint(0, len(special) - 1)])
 
-            combined = num + letter + spec
+    for _ in range(number_of_characters):
+        num = str(random.randint(0, 9))
+        letter = str(alphabeth[random.randint(0, len(alphabeth) - 1)])
+        spec = str(special[random.randint(0, len(special) - 1)])
+        combined = num + letter + spec
 
-            password += random.choice(combined)
+        password += random.choice(combined)
 
     return password
 
