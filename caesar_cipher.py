1d827ed9752766b6d931a28a625fb4941194330d
Junfel <vjunfel@gmail.com>
2025-08-30 22:20:06 +0800

    Added encryption or decryption selection option.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,10 +1,12 @@
 import string
 
-alphabet = list(string.ascii_letters)
+alphabet = list(string.ascii_letters) + list(string.printable)
 
 # alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
 #             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 #             'u', 'v', 'w', 'x', 'y', 'z']
+
+# print(alphabet)
 
 text_input = input("Type your message here: ")
 count = int(input("Shift count: "))
@@ -18,14 +20,12 @@ def encrypt(original_text, shift_count):
         
     return output
 
-print(encrypt(text_input, count).center(50, "*"))
+# print(encrypt(text_input, count).center(50, "*"))
 
 
 print("-----------------------------------------------------")
 
 # DECRYPTION
-encrypted_text = input("Type encrypted text here: ")
-
 def decrypt(encrypted_text, shift_count):
     output = ""
     
@@ -34,4 +34,22 @@ def decrypt(encrypted_text, shift_count):
     
     return output
 
-print(decrypt(encrypted_text, count))
+# print(decrypt(text_input, count))
+
+print("-----------------------------------------------------")
+# ENCRYPTION OR DECRYPTION OPTION
+
+def caesar(input_text, shift_count):
+    direction = input('Type "e" for encrypt and "d" for decrypt? ')
+    output_text = ""
+    
+    for letter in input_text:
+        if direction == "e":
+            output_text += alphabet[alphabet.index(letter) + shift_count]
+        elif direction == "d":
+            output_text += alphabet[alphabet.index(letter) - shift_count]
+            
+    return output_text
+
+
+print(caesar(text_input, count))
