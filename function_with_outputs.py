a9d309e724bc6e6b4b5619896113ccda20c7a31f
Junfel <vjunfel@gmail.com>
2025-09-06 23:56:46 +0800

    Added leaf-year checker function

--- function_with_outputs.py
+++ function_with_outputs.py
@@ -1,6 +1,23 @@
 def format_name(f_name, l_name):
     return f"{f_name} {l_name}".title()
-  
+
 
 result = format_name("junfel", "velasco")
-print(result)
+# print(result)
+
+
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return True
+    elif year % 4 == 0:
+        return True
+    elif year == 2100 or year == 1700:
+        return False
+    else:
+        return False
+        
+        
+output = is_leap_year(2020)
+print(output)
